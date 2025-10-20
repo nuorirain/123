fd7917f8a5a08eb9027b01182bb30a0f232908ca
nuorirain <seraser58snopdog@gmail.com>
2025-10-20 13:10:42 +0300

    тест-кейсы 2 и 3 в 7 задании

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -7,3 +7,14 @@ def test_check_footer_text(driver):
     text = footer.get_text()
     assert text == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.", \
         f"Ожидалось: '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.', но получено: '{text}'"
+
+
+def test_check_elements_text(driver):
+    driver.get("https://demoqa.com/")
+    button = Component(driver, (By.XPATH, "//h5[text()='Elements']"))
+    button.find_element(button.locator).click()
+    
+    center_text = Component(driver, (By.CSS_SELECTOR, "div.main-header ~ div"))
+    text = center_text.get_text()
+    assert text == "Please select an item from left to start practice.", \
+        f"Ожидалось: 'Please select an item from left to start practice.', но получено: '{text}'"
